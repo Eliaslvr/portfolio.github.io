f91fb73d0a68744cca899c3ac38dcec26b05efce
Eliaslvr <eliaslvr59@gmail.com>
2024-08-21 11:20:43 +0200

    commit

--- script.js
+++ script.js
@@ -5,22 +5,6 @@ function fetchElement() {
         for (let element of data) {
             const projet = document.querySelector('.projet');
 
-            // projet.innerHTML += `
-            //     <div class="bloc_projet">
-            //         <img src=${element.image} alt=${element.alt} />
-
-            //         <span>
-            //             <p class="bloc_title">${element.title}</p>
-            //             <p class="bloc_description">${element.description}</p>
-            //            <div>
-            //                 <button>
-            //                     <a href="${element.github}" target="_blank">Lien Github</a>
-            //                 </button>
-            //            </div>
-            //         </span>
-            //     </div>
-            // `
-
             projet.innerHTML += `
                 <div class="bloc_projet">
                     <img src=${element.image} alt=${element.alt} />
